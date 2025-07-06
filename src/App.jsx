a95c8e1849bb428cd72194d7f8be673929322b21
leon2192 <leon.strokes@gmail.com>
2025-07-06 11:33:48 -0300

    fc

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import Hero from "./components/Hero/HeroPremium"
+import Countdown from "./components/CountDown/CountDownPremium"
+import InfoEvent from "./components/InfoEvent"
+import Gallery from "./components/Gallery/GalleryPremium"
+import Gift from "./components/Gift"
+import Confirm from "./components/Confirm"
+import Footer from "./components/Footer"
+import Dresscode from "./components/Dresscode"
+import SuggestSong from "./components/SugerirCancion"
+
+function App() {
+
+
+  return (
+    <>
+    <Hero />
+    {/* <Countdown /> */}
+    <InfoEvent />
+    <Gallery/>
+    <Confirm/>
+    <Gift/>
+    
+    {/* <Dresscode/>
+    <SuggestSong/> */}
+    <Footer/>
+    </>
+  )
+}
+
+export default App
